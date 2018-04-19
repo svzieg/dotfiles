283b476f23c09388b3978693358a58b862143132
Sven Ziegler <sven.ziegler@scheer-group.com>
2018-04-19 15:04:19 +0200

    update

--- .setup/init.d/00_pacman.sh
+++ .setup/init.d/00_pacman.sh
@@ -1,1 +1,1 @@
-sudo pacman -S git pass qutebrowser tmux gvim rxvt-unicode termite imagemagick ffmpg sway rofi
+sudo pacman -S base-devel wget git pass qutebrowser tmux gvim rxvt-unicode termite imagemagick ffmpg sway rofi viewnior
--- .setup/init.d/01_aur.sh
+++ .setup/init.d/01_aur.sh
@@ -1,1 +1,25 @@
-yaourt -S --noconfirm autoenv
+# install yaourt
+if [ $(which yaourt) ]
+then
+  echo "Yaourt ist bereits installiert, update Yaourt"
+  yaourt -S --noconfirm yaourt
+else 
+  echo "Yaourt ist nicht installiert, installiere es"
+  git clone https://aur.archlinux.org/package-query.git
+  cd package-query
+  makepkg -si
+  cd ..
+  git clone https://aur.archlinux.org/yaourt.git
+  cd yaourt
+  makepkg -si
+  cd ..
+fi
+
+
+
+if [ $(which yaourt) ]
+then
+  yaourt -S --noconfirm autoenv
+else 
+  >&2 echo "ACHTUNG: YAOURT nicht gefunden"
+fi
