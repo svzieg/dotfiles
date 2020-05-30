a03f476b05b34a429ef5eb786f5b52ac3675566c
Sven Ziegler <sven.ziegler@scheer-group.com>
2020-05-30 10:17:03 +0200

    update some configs

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -21,6 +21,11 @@ if (command -v  xfce4-power-manager && ! pgrep xfce4-power-man) ; then
     xfce4-power-manager &
 fi
 
+if (command -v  pulseeffects  && ! pgrep pulseeffects) ; then
+    pulseeffects  --gapplication-service &
+fi
+
+
 run xfsettingsd
 run nm-applet
 run blueberry-tray
