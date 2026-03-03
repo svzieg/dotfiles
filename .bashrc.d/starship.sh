4c5cd4831d7d6834b6eac23d968f50241f7606de
Sven Ziegler-Köhler <sven.ziegler@scheer-group.com>
2026-03-03 16:50:34 +0100

    fix starship configs

--- .bashrc.d/starship.sh
+++ .bashrc.d/starship.sh
@@ -1,12 +1,14 @@
+STARSHIP=$(command -v starship 2>/dev/null || echo "/home/svzieg/.local/bin/starship")
 
-if [[ -z $ZSH ]]; then     
-echo "test"
-	if [ "${BASH_VERSINFO[0]}" -gt 4 ] || ([ "${BASH_VERSINFO[0]}" -eq 4 ] && [ "${BASH_VERSINFO[1]}" -ge 1 ])
-	then
-		source <("/home/svzieg/.local/bin/starship" init bash --print-full-init)
+if [[ -z $ZSH ]]; then
+	if [ "${BASH_VERSINFO[0]}" -gt 4 ] || { [ "${BASH_VERSINFO[0]}" -eq 4 ] && [ "${BASH_VERSINFO[1]}" -ge 1 ]; }; then
+		# shellcheck disable=SC1090
+		source <("$STARSHIP" init bash --print-full-init)
 	else
-		source /dev/stdin <<<"$("/home/svzieg/.local/bin/starship" init bash --print-full-init)"
+		# shellcheck disable=SC1090
+		source /dev/stdin <<<"$("$STARSHIP" init bash --print-full-init)"
 	fi
-else 
-	source <("/home/svzieg/.local/bin/starship" init zsh --print-full-init)
+else
+	# shellcheck disable=SC1090
+	source <("$STARSHIP" init zsh --print-full-init)
 fi
